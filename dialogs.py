b891f44e404b6de2587e32fe5ea94822e8b0f6e8
Alexey <support@uvviewsoft.com>
2021-04-08 13:49:04 +0300

    new opt

--- dialogs.py
+++ dialogs.py
@@ -16,10 +16,11 @@ def do_options_dlg():
     id_bk_err=1
     id_bk_warn=3
     id_bk_info=5
-    id_ev_open=6
-    id_ev_save=7
-    id_ev_ch=8
-    id_ok=9
+    id_themed=6
+    id_ev_open=7
+    id_ev_save=8
+    id_ev_ch=9
+    id_ok=10
 
     ev_line = app.ini_read(p_ini, 'events', 'cuda_lint', '')
     use_on_open = ',on_open,' in ','+ev_line+','
@@ -35,13 +36,14 @@ def do_options_dlg():
             +[c1.join(['type=check', 'pos=186,36,400,0', 'cap='+_('Colored warn &bookmarks'), 'val='+str(int(opt.color_warn_use))])]
             +[c1.join(['type=button', 'pos=6,66,180,0', 'cap='+_('Color of &infos')])]
             +[c1.join(['type=check', 'pos=186,66,400,0', 'cap='+_('Colored info boo&kmarks'), 'val='+str(int(opt.color_info_use))])]
-            +[c1.join(['type=check', 'pos=6,100,400,0', 'cap='+_('Lint on &opening file'), 'val='+str(int(use_on_open))])]
-            +[c1.join(['type=check', 'pos=6,126,400,0', 'cap='+_('Lint on &saving file'), 'val='+str(int(use_on_save))])]
-            +[c1.join(['type=check', 'pos=6,152,400,0', 'cap='+_('Lint &after text changed, and pause'), 'val='+str(int(use_on_change))])]
-            +[c1.join(['type=button', 'pos=206,182,300,0', 'cap='+_('OK'), 'ex0=1'])]
-            +[c1.join(['type=button', 'pos=306,182,400,0', 'cap='+_('Cancel')])]
+            +[c1.join(['type=check', 'pos=6,100,400,0', 'cap='+_('Use all 3 colors from syntax-theme'), 'val='+str(int(opt.colors_themed))])]
+            +[c1.join(['type=check', 'pos=6,130,400,0', 'cap='+_('Lint on &opening file'), 'val='+str(int(use_on_open))])]
+            +[c1.join(['type=check', 'pos=6,156,400,0', 'cap='+_('Lint on &saving file'), 'val='+str(int(use_on_save))])]
+            +[c1.join(['type=check', 'pos=6,182,400,0', 'cap='+_('Lint &after text changed, and pause'), 'val='+str(int(use_on_change))])]
+            +[c1.join(['type=button', 'pos=206,212,300,0', 'cap='+_('OK'), 'ex0=1'])]
+            +[c1.join(['type=button', 'pos=306,212,400,0', 'cap='+_('Cancel')])]
                         )
-        res = app.dlg_custom(_('CudaLint options'), 406, 215, text)
+        res = app.dlg_custom(_('CudaLint options'), 406, 245, text)
         if res is None: return
 
         btn, text = res
@@ -69,6 +71,7 @@ def do_options_dlg():
     opt.color_error_use = text[id_bk_err]=='1'
     opt.color_warn_use = text[id_bk_warn]=='1'
     opt.color_info_use = text[id_bk_info]=='1'
+    opt.colors_themed = text[id_themed]=='1'
 
     use_on_open = text[id_ev_open]=='1'
     use_on_save = text[id_ev_save]=='1'
--- options.py
+++ options.py
@@ -17,6 +17,7 @@ color_info = 0
 color_error_use = True
 color_warn_use = True
 color_info_use = True
+colors_themed = True
 
 use_on_open = False
 use_on_save = False
@@ -52,6 +53,7 @@ def do_options_load():
     global color_error_use
     global color_warn_use
     global color_info_use
+    global colors_themed
     global use_on_open
     global use_on_save
     global use_on_change
@@ -63,6 +65,7 @@ def do_options_load():
     color_error_use = app.ini_read(fn_ini, 'colors', 'error_use', '1')=='1'
     color_warn_use = app.ini_read(fn_ini, 'colors', 'warn_use', '1')=='1'
     color_info_use = app.ini_read(fn_ini, 'colors', 'info_use', '1')=='1'
+    colors_themed = app.ini_read(fn_ini, 'colors', 'themed', '1')=='1'
 
     use_on_open = app.ini_read(fn_ini, 'events', 'on_open', '0')=='1'
     use_on_save = app.ini_read(fn_ini, 'events', 'on_save', '0')=='1'
@@ -72,9 +75,19 @@ def do_options_apply():
     fn_warn = os.path.join(os.path.dirname(__file__), 'icons', 'bookmark_warn.png')
     fn_error = os.path.join(os.path.dirname(__file__), 'icons', 'bookmark_err.png')
 
-    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_ERROR, color_error if color_error_use else app.COLOR_NONE, fn_error)
-    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_WARN, color_warn if color_warn_use else app.COLOR_NONE, fn_warn)
-    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_INFO, color_info if color_info_use else app.COLOR_NONE, fn_warn)
+    if colors_themed:
+        data = app.app_proc(app.PROC_THEME_SYNTAX_DICT_GET, '')
+        n1 = data['LightBG1']['color_back']
+        n2 = data['LightBG2']['color_back']
+        n3 = data['LightBG3']['color_back']
+    else:
+        n1 = color_error if color_error_use else app.COLOR_NONE
+        n2 = color_warn if color_warn_use else app.COLOR_NONE
+        n3 = color_info if color_info_use else app.COLOR_NONE
+
+    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_ERROR, n1, fn_error)
+    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_WARN, n2, fn_warn)
+    app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_INFO, n3, fn_warn)
 
 def do_options_save():
     global color_error
@@ -83,6 +96,7 @@ def do_options_save():
     global color_error_use
     global color_warn_use
     global color_info_use
+    global colors_themed
     global use_on_open
     global use_on_save
     global use_on_change
@@ -94,6 +108,7 @@ def do_options_save():
     app.ini_write(fn_ini, 'colors', 'error_use', str(int(color_error_use)))
     app.ini_write(fn_ini, 'colors', 'warn_use', str(int(color_warn_use)))
     app.ini_write(fn_ini, 'colors', 'info_use', str(int(color_info_use)))
+    app.ini_write(fn_ini, 'colors', 'themed', str(int(colors_themed)))
 
     app.ini_write(fn_ini, 'events', 'on_open', str(int(use_on_open)))
     app.ini_write(fn_ini, 'events', 'on_save', str(int(use_on_save)))
