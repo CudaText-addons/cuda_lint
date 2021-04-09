24fac66045f0195015ffad8d98c574d26075e90e
Alexey <support@uvviewsoft.com>
2021-04-09 15:06:23 +0300

    rework color opts

--- dialogs.py
+++ dialogs.py
@@ -7,6 +7,24 @@ _   = get_translation(__file__)  # I18N
 
 p_ini = 'plugins.ini'
 
+COLORS = [
+    'LightBG1',
+    'LightBG2',
+    'LightBG3',
+    'LightBG4',
+    'LightBG5',
+    'IncludeBG1',
+    'IncludeBG2',
+    'IncludeBG3',
+    'IncludeBG4',
+    'SectionBG1',
+    'SectionBG2',
+    'SectionBG3',
+    'SectionBG4',
+    'CurBlockBG',
+    'SeparLine',
+    ]
+
 
 def do_options_dlg():
 
@@ -16,27 +34,40 @@ def do_options_dlg():
     id_bk_err=1
     id_bk_warn=3
     id_bk_info=5
-    id_themed=6
-    id_ev_open=7
-    id_ev_save=8
-    id_ev_ch=9
-    id_ok=10
+    id_ev_open=6
+    id_ev_save=7
+    id_ev_ch=8
+    id_ok=9
 
     ev_line = app.ini_read(p_ini, 'events', 'cuda_lint', '')
     use_on_open = ',on_open,' in ','+ev_line+','
     use_on_save = ',on_save,' in ','+ev_line+','
     use_on_change = ',on_change_slow,' in ','+ev_line+','
+    
+    try:
+        val_er = COLORS.index(opt.color_error)
+    except ValueError:
+        val_er = 0
+
+    try:
+        val_warn = COLORS.index(opt.color_warn)
+    except ValueError:
+        val_warn = 0
+
+    try:
+        val_info = COLORS.index(opt.color_info)
+    except ValueError:
+        val_info = 0
 
     while True:
         c1 = chr(1)
         text = '\n'.join([]
-            +[c1.join(['type=button', 'pos=6,6,180,0', 'cap='+_('Color of &errors')])]
+            +[c1.join(['type=combo_ro', 'pos=6,6,180,26', 'items='+'\t'.join(COLORS), 'val='+str(val_er) ])]
             +[c1.join(['type=check', 'pos=186,6,400,0', 'cap='+_('Colored e&rror bookmarks'), 'val='+str(int(opt.color_error_use))])]
-            +[c1.join(['type=button', 'pos=6,36,180,0', 'cap='+_('Color of &warns')])]
+            +[c1.join(['type=combo_ro', 'pos=6,36,180,56', 'items='+'\t'.join(COLORS), 'val='+str(val_warn) ])]
             +[c1.join(['type=check', 'pos=186,36,400,0', 'cap='+_('Colored warn &bookmarks'), 'val='+str(int(opt.color_warn_use))])]
-            +[c1.join(['type=button', 'pos=6,66,180,0', 'cap='+_('Color of &infos')])]
+            +[c1.join(['type=combo_ro', 'pos=6,66,180,86', 'items='+'\t'.join(COLORS), 'val='+str(val_info) ])]
             +[c1.join(['type=check', 'pos=186,66,400,0', 'cap='+_('Colored info boo&kmarks'), 'val='+str(int(opt.color_info_use))])]
-            +[c1.join(['type=check', 'pos=6,100,400,0', 'cap='+_('Use all 3 colors from syntax-theme'), 'val='+str(int(opt.colors_themed))])]
             +[c1.join(['type=check', 'pos=6,130,400,0', 'cap='+_('Lint on &opening file'), 'val='+str(int(use_on_open))])]
             +[c1.join(['type=check', 'pos=6,156,400,0', 'cap='+_('Lint on &saving file'), 'val='+str(int(use_on_save))])]
             +[c1.join(['type=check', 'pos=6,182,400,0', 'cap='+_('Lint &after text changed, and pause'), 'val='+str(int(use_on_change))])]
@@ -47,31 +78,18 @@ def do_options_dlg():
         if res is None: return
 
         btn, text = res
-
-        if btn==id_color_err:
-            n=app.dlg_color(opt.color_error)
-            if n is not None: opt.color_error=n
-            continue
-
-        if btn==id_color_warn:
-            n=app.dlg_color(opt.color_warn)
-            if n is not None: opt.color_warn=n
-            continue
-
-        if btn==id_color_info:
-            n=app.dlg_color(opt.color_info)
-            if n is not None: opt.color_info=n
-            continue
-
         break
 
     if btn!=id_ok: return
     text = text.splitlines()
 
+    opt.color_error = COLORS[int(text[id_color_err])]
+    opt.color_warn = COLORS[int(text[id_color_warn])]
+    opt.color_info = COLORS[int(text[id_color_info])]
+
     opt.color_error_use = text[id_bk_err]=='1'
     opt.color_warn_use = text[id_bk_warn]=='1'
     opt.color_info_use = text[id_bk_info]=='1'
-    opt.colors_themed = text[id_themed]=='1'
 
     use_on_open = text[id_ev_open]=='1'
     use_on_save = text[id_ev_save]=='1'
--- options.py
+++ options.py
@@ -10,41 +10,19 @@ KIND_ERROR = 20
 KIND_WARN = 21
 KIND_INFO = 22
 
-color_error = 0
-color_warn = 0
-color_info = 0
+color_error = 'LightBG1'
+color_warn = 'LightBG2'
+color_info = 'LightBG3'
 
 color_error_use = True
 color_warn_use = True
 color_info_use = True
-colors_themed = True
 
 use_on_open = False
 use_on_save = False
 use_on_change = False
 
 fn_ini = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'cuda_lint.ini')
-
-#------------------------
-def html_to_int(s):
-    s = s.strip()
-    if not s: return app.COLOR_NONE
-    while s[0] == '#': s = s[1:]
-    # get bytes in reverse order to deal with PIL quirk
-    if len(s)==3:
-        s = s[0]*2 + s[1]*2 + s[2]*2
-    if len(s)!=6:
-        raise Exception(_('Incorrect color token: ')+s)
-    s = s[-2:] + s[2:4] + s[:2]
-    # finally, make it numeric
-    color = int(s, 16)
-    return color
-
-def int_to_html(n):
-    s = '%06x' % n
-    r, g, b = s[4:], s[2:4], s[:2]
-    return '#'+r+g+b
-#------------------------
 
 def do_options_load():
     global color_error
@@ -53,19 +31,17 @@ def do_options_load():
     global color_error_use
     global color_warn_use
     global color_info_use
-    global colors_themed
     global use_on_open
     global use_on_save
     global use_on_change
 
-    color_error = html_to_int(app.ini_read(fn_ini, 'colors', 'error', '#ff00ff'))
-    color_warn = html_to_int(app.ini_read(fn_ini, 'colors', 'warn', '#ffff00'))
-    color_info = html_to_int(app.ini_read(fn_ini, 'colors', 'info', '#a6caf0'))
+    color_error = app.ini_read(fn_ini, 'colors', 'error_str', color_error)
+    color_warn = app.ini_read(fn_ini, 'colors', 'warn_str', color_warn)
+    color_info = app.ini_read(fn_ini, 'colors', 'info_str', color_info)
 
     color_error_use = app.ini_read(fn_ini, 'colors', 'error_use', '1')=='1'
     color_warn_use = app.ini_read(fn_ini, 'colors', 'warn_use', '1')=='1'
     color_info_use = app.ini_read(fn_ini, 'colors', 'info_use', '1')=='1'
-    colors_themed = app.ini_read(fn_ini, 'colors', 'themed', '1')=='1'
 
     use_on_open = app.ini_read(fn_ini, 'events', 'on_open', '0')=='1'
     use_on_save = app.ini_read(fn_ini, 'events', 'on_save', '0')=='1'
@@ -75,15 +51,22 @@ def do_options_apply():
     fn_warn = os.path.join(os.path.dirname(__file__), 'icons', 'bookmark_warn.png')
     fn_error = os.path.join(os.path.dirname(__file__), 'icons', 'bookmark_err.png')
 
-    if colors_themed:
-        data = app.app_proc(app.PROC_THEME_SYNTAX_DICT_GET, '')
-        n1 = data['LightBG1']['color_back'] if color_error_use else app.COLOR_NONE
-        n2 = data['LightBG2']['color_back'] if color_warn_use else app.COLOR_NONE
-        n3 = data['LightBG3']['color_back'] if color_info_use else app.COLOR_NONE
-    else:
-        n1 = color_error if color_error_use else app.COLOR_NONE
-        n2 = color_warn if color_warn_use else app.COLOR_NONE
-        n3 = color_info if color_info_use else app.COLOR_NONE
+    n1 = app.COLOR_NONE
+    n2 = app.COLOR_NONE
+    n3 = app.COLOR_NONE
+
+    data = app.app_proc(app.PROC_THEME_SYNTAX_DICT_GET, '')
+    
+    #print('er', color_error)
+    #print('w', color_warn)
+    #print('i', color_info)
+    
+    if color_error in data and color_error_use:
+        n1 = data[color_error]['color_back']
+    if color_warn in data and color_warn_use:
+        n2 = data[color_warn]['color_back']
+    if color_info in data and color_info_use:
+        n3 = data[color_info]['color_back']
 
     app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_ERROR, n1, fn_error)
     app.ed.bookmark(app.BOOKMARK_SETUP, 0, KIND_WARN, n2, fn_warn)
@@ -96,19 +79,17 @@ def do_options_save():
     global color_error_use
     global color_warn_use
     global color_info_use
-    global colors_themed
     global use_on_open
     global use_on_save
     global use_on_change
 
-    app.ini_write(fn_ini, 'colors', 'error', int_to_html(color_error))
-    app.ini_write(fn_ini, 'colors', 'warn', int_to_html(color_warn))
-    app.ini_write(fn_ini, 'colors', 'info', int_to_html(color_info))
+    app.ini_write(fn_ini, 'colors', 'error_str', color_error)
+    app.ini_write(fn_ini, 'colors', 'warn_str', color_warn)
+    app.ini_write(fn_ini, 'colors', 'info_str', color_info)
 
     app.ini_write(fn_ini, 'colors', 'error_use', str(int(color_error_use)))
     app.ini_write(fn_ini, 'colors', 'warn_use', str(int(color_warn_use)))
     app.ini_write(fn_ini, 'colors', 'info_use', str(int(color_info_use)))
-    app.ini_write(fn_ini, 'colors', 'themed', str(int(colors_themed)))
 
     app.ini_write(fn_ini, 'events', 'on_open', str(int(use_on_open)))
     app.ini_write(fn_ini, 'events', 'on_save', str(int(use_on_save)))
